86ffeca971f061aaa676cce4bab769ba0ed0e64c
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-06-05 18:58:33 +0200

    first commit

---
+++ src/Person.rb
@@ -0,0 +1,320 @@
+class Person
+  def initialize
+    #person measure target post/get method
+    @ss_addr = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/person/"
+    @ss_measureDef_addr = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/measureDefinition"
+    @ss_adapter_addr = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/adapter"
+
+    @bls_addr = "https://fierce-sea-36005.herokuapp.com/sdelab/businessLogic-service/person/"
+
+    #post method
+    @pcs_addr = "https://desolate-thicket-56593.herokuapp.com/sdelab/processCentric-service/person/"
+
+  end
+
+  def help()
+    help= '
+          You can control me by sending these commands:\n
+          /searchPerson - search person
+          personDetails - view person details
+          measureInfo - view list of all measures
+          goalInfo - view list of goals
+          currentMeasureList - view list of current measure
+          p -checkMeasureList - view list of given measure
+          p -verifyGoal - check if goal achieved
+          p -comparisonInfo - comparison value of measure and goal
+          p -createMeasure <name> <value> - create new measure
+          p -createGoal <type> <value> <starDateGoal> 
+                        <endDateGoal><achieved> - create new goal
+          p -createPerson <firstname> <lastname> <birthdate> 
+                          <email> <gender> - create new person
+          pDelete <idPerson> - delete a person'
+    help
+  end
+
+  #RestClient.post "http://example.com/resource", { 'x' => 1 }.to_json, :content_type => :json, :accept => :json
+  # create person method
+  public
+
+  def createPerson(firstname,lastname,birthdate,email,gender)
+    addr = @ss_addr.to_s
+
+    puts addr
+    puts "Inside the method createPerson !!! "
+    sez = {'firstname' => firstname,'lastname' => lastname,'birthdate' => birthdate,'email' => email,'gender' => gender}
+    puts sez
+    response = RestClient.post addr.to_s, {'firstname' => firstname,'lastname' => lastname,'birthdate' => birthdate,'email' => email,'gender' => gender}.to_json,
+    :content_type => :json, :accept => 'application/json'
+    
+    puts response
+    newPerson = response.to_s
+    puts "Id new person: " + newPerson.to_s
+    text = "Id new person: " + newPerson.to_s
+    return text
+  end
+
+  # create goal method
+  public
+
+  def createGoal(personId,type,value,startDateGoal,endDateGoal,achieved)
+    addr = @ss_addr.to_s + personId.to_s + "/goal"
+    puts addr
+    puts "Inside the method createGoal !!! "
+
+    sez = {'type' => type,'value' => value,'startDateGoal' => startDateGoal,'endDateGoal' => endDateGoal,'achieved' => achieved}
+    puts sez
+    response = RestClient.post addr.to_s, {'type' => type,'value' => value,'startDateGoal' => startDateGoal,'endDateGoal' => endDateGoal,'achieved' => achieved}.to_json,
+    :content_type => :json, :accept => 'application/json'
+
+    puts response
+    newGid = response.to_s
+    puts "Id new goal: " + newGid.to_s
+
+    #calls checkNewGoal method --> PCS
+    addr = @pcs_addr.to_s + personId.to_s + "/checkNewGoal/" + newGid.to_s
+    puts addr
+    puts "Inside the method checkNewGoal !!! "
+
+    response = RestClient.get addr
+    result = JSON.parse(response)
+    newGoal = result["newGoal"]
+    text = "Goal: " + newGoal['name'].to_s + "\n Type: " + newGoal['type'].to_s + "\n Value: " + newGoal['value'].to_s + "\n StartDateGoal: " + newGoal['startDateGoal'].to_s + "\n EndDateGoal: " + newGoal['endDateGoal'].to_s + "\n Achieved: " + newGoal['achieved'].to_s
+    return text
+  end
+
+  # create measure method
+  public
+
+  def createMeasure(personId,name,value)
+    #calls createMeasure method --> SS
+    addr = @ss_addr.to_s + personId.to_s + "/measure"
+    puts addr
+    puts "Inside the method createMeasure !!! "
+
+    sez = {'name' => name,'value' => value}
+    puts sez
+    response = RestClient.post addr.to_s, {'name' => name,'value' => value}.to_json,
+    :content_type => :json, :accept => 'application/json'
+
+    puts response
+    newMid = response.to_s
+    puts "Id new measure: " + newMid.to_s
+
+    #calls checkNewMeasure method --> PCS
+    addr = @pcs_addr.to_s + personId.to_s + "/checkNewMeasure/" + newMid.to_s
+    puts addr
+    puts "Inside the method checkNewMeasure !!! "
+
+    response = RestClient.get addr
+    result = JSON.parse(response)
+    newMeasure = result["newMeasure"]
+    text = "Measure: " + newMeasure['name'].to_s + "\n Type: " + newMeasure['type'].to_s + "\n Value: " + newMeasure['value'].to_s + "\n Created: " + newMeasure['created'].to_s
+    return text
+  end
+
+  # view person Details method
+  public
+
+  def viewPersonDetails(personId)
+    addr = @bls_addr.to_s + personId.to_s
+    puts addr
+    puts "Inside the method viewPersonDetails !!! "
+    response = RestClient.get addr
+    puts response
+
+    person = JSON.parse(response)
+    text = "Firstname: "+person['firstname']+"\n Lastname: "+person['lastname']+ "\n Birthdate: "+ person['birthdate']+"\n Email: "+person['email']+"\n Gender: "+person['gender']
+    return text
+  end
+
+  # view list of goal method
+  public
+
+  def showListGoal(personId)
+    addr = @bls_addr.to_s + personId.to_s+"/goal"
+    puts addr
+    puts "Inside the method showListGoal !!! "
+
+    response = RestClient.get addr
+    if response.code == 200
+      puts response
+      result = JSON.parse(response)
+      x=result['goal']
+      if !x.empty?
+        text = " "
+        x.each do |el|
+          text << "\n Goal: " + el['type'].to_s + "\n Value: " + el['value'].to_s + "\n Start Date: " + el['startDateGoal'].to_s + "\n End Date: " + el['endDateGoal'].to_s + "\n Achieved: " + el['achieved'].to_s + " \n "
+        end
+      else
+        text = "There are not goals"
+      end
+    else
+      text = "Error"
+    end
+    return text
+  end
+
+  # view list of measure types method
+  public
+
+  def showMeasureDefinition
+    addr = @ss_measureDef_addr
+    puts addr
+    puts "Inside the method view list of measure definition !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    puts result
+    measureName = result['measureName']
+    text = " "
+    measureName.each do |el|
+      text << "\n MeasureName: " + el
+    end
+    return text
+  end
+
+  # view list of current measure method
+  public
+
+  def showListCurrentHealth(personId)
+    addr = @bls_addr.to_s + personId.to_s+"/current-health"
+    puts addr
+    puts "Inside the method view list of current measure !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    puts result
+    currentHealth = result['currentHealth-profile']
+    measure = currentHealth['measure']
+    text = " "
+    measure.each do |el|
+      text << "\n Measure: "+el['name'].to_s + "\n Value: "+el['value'].to_s + "\n Created: "+el['created'].to_s + "\n"
+    end
+    return text
+  end
+
+  # view list of history measure method
+  public
+
+  def showListHistoryHealth(personId)
+    addr = @bls_addr.to_s + personId.to_s+"/history-health"
+    puts addr
+    puts "Inside the method view list of history measure !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    puts result
+    historyHealth = result['historyHealth-profile']
+    measure = historyHealth['measure']
+    text = " "
+    measure.each do |el|
+      text << "\n Measure: "+el['name'].to_s + "\n Value: "+el['value'].to_s + "\n Created: "+el['created'].to_s + "\n"
+    end
+    return text
+  end
+
+  # view list of a given measure
+  public
+
+  def checkMeasureList(personId,measureName)
+    addr = @bls_addr.to_s + personId.to_s + "/measure/"+ measureName.to_s
+    puts addr
+    puts "Inside the method check list of a given measure !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    puts result
+    measureProfile = result['measure-profile']
+    measure = measureProfile['measure']
+    text = "Measure: " + measureName.to_s + "\n"
+    measure.each do |el|
+      text << "\n Mid: " + el['mid'].to_s + "\n Value: " + el['value'].to_s + "\n Created: " + el['created'].to_s + "\n"
+    end
+    return text
+  end
+
+  #Delete person method
+  public
+
+  def deletePerson(personId)
+    addr = @ss_addr.to_s + personId.to_s
+    puts addr
+    puts "Inside the method deletePerson !!! "
+    response = RestClient.delete addr
+    if response.code === 204
+      text = "Person with " + personId.to_s + " deleted."
+    else
+      text = "Person with " + personId.to_s + " does not exist."  
+    end
+  end
+
+  # motivation quote method
+  public
+
+  def getQuote
+    addr = @ss_adapter_addr + "/quote"
+    puts addr
+    puts "Inside the method getQuote !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    quote = result['result']
+    text = " "
+    text << "\n Quote: " + quote['quote'].to_s + "\n Author: " + quote['author'].to_s
+    return text
+  end
+
+  # comparison information about measure and goal method
+  public
+
+  def comparisonInfo(personId,measureName)
+    addr = @pcs_addr + personId.to_s + "/comparisonInfo/" + measureName.to_s
+    puts addr
+    puts "Inside the method getComparisonInfo !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    info = result['info']
+    measure = info['measure']
+    goal = info['goal']
+    comparison = info['comparison']
+    text = " "
+    text << "\n Measure: " + measure['name'].to_s + "\n Type: " + measure['type'].to_s + "\n Value: " + measure['value'].to_s + "\n" +
+    "\n Goal: " + goal['name'].to_s + "\n Value: " + goal['value'].to_s + "\n Achieved: " + goal['achieved'].to_s + "\n" +
+    "\n Result: " + comparison['result'].to_s + "\n Quote: " + comparison['quote'].to_s
+    return text
+  end
+
+  # verify if goal achieved
+  public
+
+  def verifyGoal(personId,measureName)
+    addr = @pcs_addr + personId.to_s + "/verifyGoal/" + measureName
+    puts addr
+    puts "Inside the method verifyGoal !!! "
+    response = RestClient.get addr
+    puts response
+
+    result = JSON.parse(response)
+    check = result['verifyGoal']
+    goal = check['goal']
+    text = " "
+    text << "\n Goal: " + goal['name'].to_s + "\n Type: " + goal['type'].to_s + "\n Value: " + goal['value'].to_s + "\n Achieved: " + goal['achieved'].to_s + "\n Motivation: " + goal['motivation'].to_s
+    return text
+  end
+
+  def bls_addr
+    @bls_addr
+  end
+
+  def bls_addr=(bls)
+    @bls_addr=bls
+  end
+
+end
---
+++ src/bot.rb
@@ -0,0 +1,319 @@
+require 'telegram/bot'
+require 'rest-client'
+require 'pp'
+require 'faraday'
+
+require_relative 'Person'
+
+token = '217842946:AAFyBzppkNaN70JXGlQzXVTBB9XVwKnfCc4'
+
+$id_Person = 0
+$measureName = ""
+$mode = "json"
+
+puts '---------------------------------'
+puts 'Starting the bot...'
+puts '---------------------------------'
+
+# Storage Services
+ss_addr = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/person/"
+# Business Logic Services
+bls_addr = "https://fierce-sea-36005.herokuapp.com/sdelab/businessLogic-service/person/"
+# Process Centric Services
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+
+    #**************************** START - STOP - HELP ********************************************
+    when '/start'
+      question = '
+       Welcome, to LIFECOACH SYSTEM!
+
+       This application will help you to control your
+       everyday physical activity and hydration.
+       Measure the number of steps per day and
+       how many litres of water you drink and
+       see if it is enough for balanced healthy life.
+
+       Send /searchPerson to begin the application.
+       Tell the bot to /stop when
+       you are done.
+       Send /help if you want to see the menu.'
+
+      answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(/searchPerson)], one_time_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+      #bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}", reply_markup: answers)
+
+    when '/stop'
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+      #bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+
+    when '/help'
+      obj_person = Person.new
+      obj_person.help()
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: obj_person.help(), reply_markup: kb)
+
+      #      #**************************** LOGIN ********************************************
+
+    when '/searchPerson'
+      ask_id = "Welcome: insert your p=<id>"
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: ask_id, reply_markup: kb)
+
+      #Check id for person
+    when /p=\d+/
+      id = message.text.match(/\d+/)[0].to_i
+      $id_Person = id
+      puts "Id person: " + $id_Person.to_s
+      puts id.to_s
+      response = RestClient.get 'https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/person/'+$id_Person.to_s
+      if response.code != 200
+        error_message = "Wrong id... Please search your id."
+        bot.api.send_message(chat_id: message.chat.id, text: error_message)
+      else
+        welcome_person = "Push personDetails button to receive last info about you"
+        answers_user = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [['welcome', '/help', '/stop'],
+          ['personDetails', 'measureInfo',  'goalInfo'],
+          ['currentMeasureList', 'p -checkMeasureList'],
+          ['p -verifyGoal', 'p -comparisonInfo'],
+          ['p -cMeasure', 'p -cGoal', 'pDelete']], one_time_keyboard: false)
+        bot.api.send_message(chat_id: message.chat.id, text: welcome_person, reply_markup: answers_user)
+      end
+
+      #**************************** PERSON ********************************************
+      #Create new person
+      #p -cPerson Jack Lambert 2000-12-10 hgjfoe@hotmail.com M
+    when /p\s-cPerson/
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b: " + b.size.to_s
+
+      if b.size === 7
+        firstname = b[2]
+        lastname = b[3]
+        birthdate = b[4]
+        email = b[5]
+        gender = b[6]
+
+        puts "Firstname: " + firstname
+        puts "Lastname: " + lastname
+        puts "Birthdate: " + birthdate
+        puts "Email: " + email
+        puts "Gender: " + gender
+
+        obj_person = Person.new()
+        text = obj_person.createPerson(firstname,lastname,birthdate,email,gender)
+        puts "Response of method post in bot: "+ text.to_s
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+        bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      else
+        puts "You just making it up!"
+      end
+
+      #Create new goal
+      #p -cGoal water 4 2016-05-09 2016-05-20 false
+    when /p\s-cGoal/
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b " + b.size.to_s
+      if b.size === 7
+        type = b[2]
+        value = b[3]
+        startDateGoal = b[4]
+        endDateGoal = b[5]
+        achieved = b[6]
+
+        puts "Type: " + type
+        puts "Value: " + value
+        puts "StartDateGoal: " + startDateGoal
+        puts "EndDateGoal: " + endDateGoal
+        puts "Achieved: " + achieved
+        obj_person = Person.new()
+        text = obj_person.createGoal($id_Person,type,value,startDateGoal,endDateGoal,achieved)
+        puts "Response of method post in bot: "+ text.to_s
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+        bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+      else
+        puts "You just making it up!"
+      end
+
+      #Create new measure
+      #p -cMeasure water 4
+    when /p\s-cMeasure/
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b " + b.size.to_s
+      if b.size === 4
+        name = b[2]
+        value = b[3]
+
+        puts "Name: " + name
+        puts "Value: " + value
+        obj_person = Person.new()
+        text = obj_person.createMeasure($id_Person,name,value)
+        puts "Response of method post in bot: "+ text.to_s
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+        bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+      else
+        puts "You just making it up!"
+      end
+
+    when 'welcome'
+      puts "Id person inside if welcome " + $id_Person.to_s
+      welcome_message = "Welcome back, "
+      response = RestClient.get 'https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service/person/'+$id_Person.to_s
+      person = JSON.parse(response)
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      text = welcome_message + person['lastname'] + ' ' + person['firstname'] + '!. Please, choose an operation from the MENU to proceed'
+      bot.api.send_message(chat_id: message.chat.id, text: text , reply_markup: kb)
+
+      #show person detail
+    when 'personDetails'
+      puts $id_Person != 0
+      if $id_Person != 0
+        puts "Id person inside if personInfo " + $id_Person.to_s
+        obj_person = Person.new()
+        text = obj_person.viewPersonDetails($id_Person)
+        puts text.to_s
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+        bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+      end
+
+      #Get list of goal
+    when 'goalInfo'
+      if $id_Person != 0
+        puts 'Inside show list of goal...'
+        obj_person = Person.new
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+        bot.api.send_message(chat_id: message.chat.id, text: obj_person.showListGoal($id_Person), reply_markup: kb)
+      end
+
+      #Get list of measure types
+    when 'measureDefinition'
+      puts 'Inside list of measure types'
+      obj_person = Person.new
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: obj_person.showMeasureDefinition, reply_markup: kb)
+
+      #Get list of current measure
+    when 'currentMeasureList'
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      puts 'Inside show list of current measure...'
+      obj_person = Person.new()
+      text = obj_person.showListCurrentHealth($id_Person)
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      #Get list of history measure
+    when 'measureInfo'
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      puts 'Inside show list of history measure...'
+      obj_person = Person.new()
+      text = obj_person.showListHistoryHealth($id_Person)
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      #p -checkMeasureList weight
+    when /p\s-checkMeasureList/
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      puts 'Inside check a list of a given measure method...'
+
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b: " + b.size.to_s
+      puts "b[2]: " + b[2]
+      if b.size === 3
+        measureName = b[2]
+        puts "MeasureName: " + measureName
+      end
+      obj_person = Person.new()
+      text = obj_person.checkMeasureList($id_Person,measureName)
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+    when 'getQuote'
+      puts 'Inside getQuote...'
+      obj_person = Person.new()
+      text = obj_person.getQuote
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      #p -comparisonInfo weight
+    when /p\s-comparisonInfo/
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      puts 'Inside comparison information method...'
+
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b: " + b.size.to_s
+      puts "b[2]: " + b[2]
+      if b.size === 3
+        measureName = b[2]
+        puts "MeasureName: " + measureName
+      end
+      obj_person = Person.new()
+      text = obj_person.comparisonInfo($id_Person,measureName)
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      #p -verifyGoal weight
+    when /p\s-verifyGoal/
+      if $id_Person == 0
+        $id_Person = 1
+      end
+
+      puts 'Inside verify goal method...'
+
+      b=message.text.gsub(/\s+/m, ' ').strip.split(" ")
+      puts "Size of b: " + b.size.to_s
+      puts "b[2]: " + b[2]
+      if b.size === 3
+        measureName = b[2]
+        puts "MeasureName: " + measureName
+      end
+      obj_person = Person.new()
+      text = obj_person.verifyGoal($id_Person,measureName)
+      puts text.to_s
+      kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: false)
+      bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+
+      #Delete Person
+      #pDelete <idPerson>
+    when /pDelete \d+/
+      id = message.text.match(/\d+/)[0].to_i
+      if id != 0
+        puts "Inside delete method"
+        obj_person = Person.new()
+        text = obj_person.deletePerson(id)
+        puts text.to_s
+        kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+        bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: kb)
+      end
+
+    end
+  end
+end
